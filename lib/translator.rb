a8bcfde973ba27b8590379b25d6a03fe15b0225f
Lindsay Pierce <lindsay.pierce@yale.edu>
2020-05-22 13:44:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,53 @@
 # require modules here
-
-def load_library
+require 'pry'
+require 'yaml'
+def load_library (file_path)
   # code goes here
+  
+  library_hash = YAML.load(File.read(file_path))
+  # binding.pry
+  
+  # hash.each_with_object({}) do |(k, v), a|
+     # a[k] = v + 3
+ # end                   # => { a => 5, b => 6 }
+  
+  library_hash.each_with_object({}) do |(key, value), final_library|
+    
+    if !final_library[key]
+      final_library[key] = {:english => nil, :japanese => nil}
+    end
+    
+    
+    final_library[key].each_with_index do |(language, labeled_emoticon), outer_index|
+      if !labeled_emoticon
+        value.each_with_index do |og_emoticon, index|
+          if outer_index == index
+            final_library[key][language] = og_emoticon
+            # binding.pry
+          end
+        end
+      end
+    end
+  end
+  
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon (file_path, e_emoticon)
   # code goes here
+  library = load_library(file_path)
+ # binding.pry
+  
 end
 
-def get_english_meaning
+def get_english_meaning (file_path, j_emoticon)
   # code goes here
+  library = load_library(file_path)
+  
+  meaning_array = library.select do |meaning, symbols|
+    symbols[:japanese] == j_emoticon
+  end
+  # binding.pry
+  
+  meaning_array.first[0]
+  # binding.pry
 end
